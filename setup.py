2279c6d00dc8e1a2bce60ee4ba60ecc39f98c8ad
matt wilkie <maphew+github@gmail.com>
2015-01-28 23:25:14 -0800

    start following packaging guide
    ...almost embarrassed to commit this, so negligible a change! ...but
    want my tree clean so I can start catching up with master again

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
 #@+leo-ver=5-thin
 #@+node:maphew.20141126130213.2: * @file ../../setup.py
-from distutils.core import setup
+from setuptools import setup, find_packages # Always prefer setuptools over distutils
+from codecs import open # To use a consistent encoding
 import leo.core.leoVersion
 #@+others
 #@+node:maphew.20141126230535.3: ** docstring
 '''setup.py for leo
 
     Nov 2014: strip to bare minimum and rebuild using ONLY
-    https://docs.python.org/2/distutils/setupscript.html 
+    https://python-packaging-user-guide.readthedocs.org/en/latest/index.html
     
     See if we can get all the way to end (painless `python setup.py install` and
     `pip install .` on all platforms) without having to leave standard library
-    or leverage hacks.    
+    or leverage hacks.
 '''
 #@+node:maphew.20141126230535.4: ** classifiers
 classifiers = [
@@ -44,6 +45,7 @@ setup(
     platforms = ['Linux','Windows','MacOS'],
     download_url = 'http://sourceforge.net/projects/leo/files/Leo/',
     classifiers = classifiers,
+    packages = find_packages()
     #packages = ['', 'core'], 
     #package_dir = {'': 'leo'},
 )
